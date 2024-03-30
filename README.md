7999776151aae01a47211f19c44a78e3c61eab14
Federico-Gz <federicograziani96@gmail.com>
2024-03-30 11:04:53 +0100

    add comment in index.js file

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# advanced-javascript-project
+# TA - Tech Alert
+
+Welcome to TA, where you can read about the latest tech news
+
+![schermata di gioco](src/img/app-screen.png)
+
+---
+
+## HOW IT WORKS
+
+At the start, you will able to see the latest 10 news's title obtained from the service 'Hacker News'.
+
+If you want read more about every single news just click on the "Read More" button, which allows you to access to the full news.
+
+Click on the "Load More" button to load the next 10 news and scroll to see it.
+
+---
+
+## Used Technologies
+
+- HTML
+- CSS
+- JavaScript
+- Webpack
+- Axios
+- dotenv-webpack
+
+---
+
+## Struttura progetto
+
+- index.html: contiene la struttura html del gioco;
+- assets/css/styles.css: file css per gli stili;
+- assets/js/script.js: file Javascript per la logica del gioco;
+- assets/img/: cartella contenente l'immagine di sfondo del gioco.
+
+---
+
+## How To Use
+
+> Clone this repository on your PC:
+  git repo clone Federico-Gz/javascript-advanced-project
+> Go the site:
+  https://js-basics-a9e4b.web.app/
--- src/js/index.js
+++ src/js/index.js
@@ -1,36 +1,48 @@
+// Import axios for HTTP requests 
 import axios from 'axios';
+
+// Impotr CSS file
 import '../css/style.css';
 
+// Start execution when DOM content is loaded
 document.addEventListener("DOMContentLoaded", function() {
+    //Get references to the DOM elements
     const newsSection = document.getElementById("news-section");
     const newsBoxes = document.getElementById("news-boxes");
     const loadMoreBtn = document.getElementById("load-more-btn");
     let startIndex = 0;
     let endIndex = 10;
 
-    
+    // Function which calls API to get first ten news
     function loadNewsStories(){
+        //GET request to the API
         axios.get(`${process.env.API_URL}/newstories.json`)
             .then(response => {
+                //Extract the news IDs
                 const newsIds = response.data.slice(startIndex, endIndex);
                 loadNewsDetails(newsIds);
             })
-            .catch(error => console.error('Errore nel recupero degli ID', error));
+            .catch(error => console.error('Errore getting ID', error));
     }
 
+    //Initial loading of news stories when the page loads
     loadNewsStories();
 
-    
+    // Function which calls API to get news details and  appends them to the page
     function loadNewsDetails(newsIds){
+        // iterate for each news ID
         newsIds.forEach(newsId => {
+            //GET request to the API for the news details
             axios.get(`${process.env.API_URL}/item/${newsId}.json`)
                 .then(response => {
                     const news = response.data;
+                    //create HTML element to display the news with details
                     const newsBox = document.createElement('div');
                     const newsBoxTitle = document.createElement('div');
                     const newsBoxTime = document.createElement('div');
                     const newsBoxLink = document.createElement('a'); 
-    
+                    
+                    // Add class, attributes and append the elements
                     newsBox.classList.add('news-box', 'quicksand-2');
     
                     newsBoxTitle.classList.add('box-title');
@@ -50,13 +62,16 @@ document.addEventListener("DOMContentLoaded", function() {
     
                     newsBoxes.appendChild(newsBox);
                 })
-                .catch(error => console.error('Errore nel recupero dei dettagli della notizia:', error));
+                .catch(error => console.error('Error getting news details:', error));
         });
     }
 
+    //Event listener for the "Load More" button
     loadMoreBtn.addEventListener('click', ()=>{
+        //Increment start and end index to load tnext ten news
         startIndex += 10;
         endIndex += 10;
+        //Load new news
         loadNewsStories();
     })
 });
