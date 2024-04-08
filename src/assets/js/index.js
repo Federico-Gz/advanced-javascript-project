6b4e09ab002ba92441bfe982d15c538c28d57801
Federico-Gz <federicograziani96@gmail.com>
2024-04-08 13:20:35 +0200

    added Lodash

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,12 +1,15 @@
-// Import axios for HTTP requests 
+// Import Axios for HTTP requests
 import axios from 'axios';
 
-// Import css file
+// Import Lodash 
+import _ from 'lodash';
+
+// Import CSS file
 import '../../assets/css/style.css';
 
 // Start execution when DOM content is loaded
 document.addEventListener("DOMContentLoaded", function() {
-    //Get references to the DOM elements
+    // Get references to the DOM elements
     const newsBoxes = document.getElementById("news-boxes");
     const loadMoreBtn = document.getElementById("load-more-btn");
     let startIndex = 0;
@@ -28,12 +31,12 @@ document.addEventListener("DOMContentLoaded", function() {
         // Add class, attributes and append the elements
         newsBox.classList.add('news-box', 'quicksand-2');
         newsBoxTitle.classList.add('box-title');
-        newsBoxTitle.textContent = news.title;
+        newsBoxTitle.textContent = _.get(news, 'title', 'Untitled');
         newsBoxTime.classList.add('box-time');
-        newsBoxTime.textContent = new Date(news.time * 1000).toLocaleString();
+        newsBoxTime.textContent = new Date(_.get(news, 'time', 0) * 1000).toLocaleString();
         newsBoxLink.classList.add('box-link', 'quicksand-1');
         newsBoxLink.textContent = "Read More";
-        newsBoxLink.href = news.url;
+        newsBoxLink.href = _.get(news, 'url', '#');
         newsBoxLink.target = "_blank";
     
         newsBox.appendChild(newsBoxTitle);
@@ -43,20 +46,21 @@ document.addEventListener("DOMContentLoaded", function() {
         return newsBox;
     }
     
+    // Function to load news stories
     function loadNewsStories() {
         // GET request to fetch news IDs
         axios.get(`${process.env.API_URL}/newstories.json`)
             .then(response => {
                 // Extract the news IDs
-                const newsIds = response.data.slice(startIndex, endIndex);
+                const newsIds = _.slice(response.data, startIndex, endIndex);
                 // Create an array of promises for fetching news details
-                const promises = newsIds.map(newsId => fetchNewsDetails(newsId));
+                const promises = _.map(newsIds, newsId => fetchNewsDetails(newsId));
                 // Wait for all promises to resolve using Promise.all
                 return Promise.all(promises);
             })
             .then(newsResponses => {
                 // Process each news detail response
-                newsResponses.forEach(response => {
+                _.forEach(newsResponses, response => {
                     const news = response.data;
                     // Create news box HTML element
                     const newsBox = createNewsBox(news);
